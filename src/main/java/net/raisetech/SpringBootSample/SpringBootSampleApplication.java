b96efb1e6985d4e34679e8509b820b0076ca8644
Daisuke943 <oosabeli1@gmail.com>
2022-09-12 23:57:13 +0900

    第6回課題実施

--- src/main/java/net/raisetech/SpringBootSample/SpringBootSampleApplication.java
+++ src/main/java/net/raisetech/SpringBootSample/SpringBootSampleApplication.java
@@ -2,14 +2,32 @@ package net.raisetech.SpringBootSample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
+@RestController
 public class SpringBootSampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSampleApplication.class, args);
 	}
 
-	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+	@GetMapping("/japan")
+	public String japan(@RequestParam(value = "name", defaultValue = "日本") String name) {
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+		LocalDateTime time = LocalDateTime.now();
+
+		return String.format("%sの現在時刻は%sです。", name, time.format(fmt));
+	}
 
 }
