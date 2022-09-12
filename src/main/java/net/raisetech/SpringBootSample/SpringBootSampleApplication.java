56f608f4cf5a8f38de3250dd62d957a894c37bae
Daisuke943 <oosabeli1@gmail.com>
2022-09-12 23:13:18 +0900

    test

---
+++ src/main/java/net/raisetech/SpringBootSample/SpringBootSampleApplication.java
@@ -0,0 +1,15 @@
+package net.raisetech.SpringBootSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSampleApplication.class, args);
+	}
+
+	
+
+}
