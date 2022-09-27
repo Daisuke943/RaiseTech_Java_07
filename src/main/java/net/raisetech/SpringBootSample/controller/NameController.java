3f3fd8acde21307befa9927de70ef868e8b28332
Daisuke943 <oosabeli1@gmail.com>
2022-09-27 23:23:39 +0900

    レビュー指摘対応

--- src/main/java/net/raisetech/SpringBootSample/controller/NameController.java
+++ src/main/java/net/raisetech/SpringBootSample/controller/NameController.java
@@ -20,10 +20,9 @@ import java.util.Optional;
 @RestController
 public class NameController {
     @GetMapping("/names")
-    public ResponseEntity<Map<String, String>> getNames(@RequestParam(value = "name", required = false) String name) {
-        Optional<String> namenull = Optional.ofNullable(name);
-        if(namenull.isEmpty()) {
-            return ResponseEntity.ok(Map.of("message", "リクエストパラメータに値を設定してください"));
+    public ResponseEntity<Map<String, String>> getNames(@RequestParam(value = "name", defaultValue = "name") String name) {
+        if("name".equals(name)) {
+            return ResponseEntity.ok(Map.of("message", "nameに指定された人物が存在しません"));
         } else {
             return ResponseEntity.ok(Map.of("message", name));
         }
@@ -31,16 +30,17 @@ public class NameController {
 
     @PostMapping("/names")
     public ResponseEntity<String> create(@RequestBody CreateForm form) {
-        URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
+        String urlStr = "http://localhost:8080";
+        URI url = UriComponentsBuilder.fromUriString(urlStr)
                 .path("/names/id")
                 .build()
                 .toUri();
-        return ResponseEntity.created(url).body("name successfully created");
+        return ResponseEntity.created(url).body("name successfully created URL:" + url);
     }
 
     @PatchMapping("/names/{id}")
     public ResponseEntity<Map<String, String>> update(@PathVariable("id") int id, @RequestBody UpdateForm form) {
-        return ResponseEntity.ok(Map.of("message", "name successfully updated"));
+        return ResponseEntity.ok(Map.of("message", "name successfully updated. name:" + form.getName()));
     }
 
     @DeleteMapping("/names/{id}")
