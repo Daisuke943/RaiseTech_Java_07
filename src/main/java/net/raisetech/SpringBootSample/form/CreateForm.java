9017dd6c229ff13776ad6903e82798ab530ef60d
Daisuke943 <oosabeli1@gmail.com>
2022-10-29 23:39:12 +0900

    第10回課題提出

--- src/main/java/net/raisetech/SpringBootSample/form/CreateForm.java
+++ src/main/java/net/raisetech/SpringBootSample/form/CreateForm.java
@@ -1,14 +1,12 @@
 package net.raisetech.SpringBootSample.form;
 
+import lombok.Getter;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-
+@Getter
 public class CreateForm {
     @NotBlank
     @Size(max = 19)
     private String name;
-
-    public String getName() {
-        return name;
-    }
 }
